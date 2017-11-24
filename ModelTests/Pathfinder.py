3a92d62ba1bc8a78053068c0e24dbfe26c87be40
Ugdha <alexisvlrt@gmail.com>
2017-11-24 12:45:39 +0100

    Pathfinding done

--- ModelTests/Pathfinder.py
+++ ModelTests/Pathfinder.py
@@ -1,10 +1,12 @@
 from Model.Pathfinder import PathFinder
 
-pf = PathFinder((-2, -22), (2, -18), 15, 500, 1)
+pf = PathFinder((-2, -23), (8, -13), 15, 500, 1)
 pf.get_path()
 pf.get_map_change_coords()
 pf.add_path_to_glued_maps()
 pf.add_map_change_ccords_to_glued_maps()
-pf.map_to_image(pf.glued_maps, 10)
+pf.map_to_image(pf.glued_maps, 1)
+print(pf.map_change_coords)
+print(pf.get_map_change_cells())
 __author__ = 'Alexis'
 
