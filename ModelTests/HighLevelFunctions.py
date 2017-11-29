b54ba46a81b46248304e50151c2ee6ad18626def
Ugdha <alexisvlrt@gmail.com>
2017-11-29 11:16:24 +0100

    Added harvest function

--- ModelTests/HighLevelFunctions.py
+++ ModelTests/HighLevelFunctions.py
@@ -1,7 +1,19 @@
 from Model.HighLevelFunctions import HighLevelFunctions
+import time
 
 hf = HighLevelFunctions(0)
 hf.interface.connect('wublel7', 'wubwublel7', 'Dihydroquerina')
-hf.goto((-2, -24))
 
+'''
+for i in range(7):
+    hf.goto((6-1-i, -24))
+    with open('res_log.text', 'a') as f:
+        f.write(str(hf.interface.get_map_resources())+'\n')
+'''
+hf.goto((-1, -2), worldmap=2)
+hf.harvest_map()
+
+
+while 1:
+    time.sleep(1)
 __author__ = 'Alexis'
