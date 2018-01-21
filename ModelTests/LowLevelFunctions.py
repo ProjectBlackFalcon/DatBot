5bfdc69a2d04fc27ae4126343cc2260e14e6e45a
Ugdha <alexisvlrt@gmail.com>
2018-01-21 19:37:13 +0100

    Zaap related tests

--- ModelTests/LowLevelFunctions.py
+++ ModelTests/LowLevelFunctions.py
@@ -3,8 +3,9 @@ import time
 
 print(LowLevelFunctions().cell2coord(555))
 print(LowLevelFunctions().coord2cell((29, 23)))
-
 print(LowLevelFunctions().distance_cell(0, 14))
-
 print(LowLevelFunctions().get_neighbour_cells(14))
+LowLevelFunctions().add_discovered_zaap('bot_name', (-3, -42))
+LowLevelFunctions().add_discovered_zaap('bot_name', (-27, -36))
+LowLevelFunctions().add_discovered_zaap('bot_name', (-3, -45))
 __author__ = 'Alexis'
