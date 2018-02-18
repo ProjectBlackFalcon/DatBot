02b4ac070f5ad612accee1f41b9cc2735634cc84
Baptiste <baptiste.beduneau@reseau.eseo.fr>
2018-02-18 22:51:15 +0100

    Fix abandonHunt

--- DatBot.Interface/src/main/Test.java
+++ DatBot.Interface/src/main/Test.java
@@ -32,11 +32,11 @@ public class Test {
 		// communication.getReturn("2;0;i;cmd;connect;[wublel7,wubwublel7,Dihydroquerina,Julith]");
 		// communication.getReturn("3;0;i;cmd;connect;['wublel6','32407c62d2f','Pot-ator','Julith']");
 		communication.getReturn("0;0;i;cmd;connect;[Jemappellehenry2,azerty123henry,Baddosch,Julith]");
-		communication.getReturn("0;0;i;cmd;checkPhorror;[None]");
-		communication.getReturn("0;0;i;cmd;getHuntStart;[None]");
-		communication.getReturn("0;0;i;cmd;getCluesLeft;[None]");
-		communication.getReturn("0;0;i;cmd;getStepsLeft;[None]");
-		communication.getReturn("0;0;i;cmd;huntFight;[None]");
+		communication.getReturn("0;0;i;cmd;abandonHunt;[None]");
+//		communication.getReturn("0;0;i;cmd;getHuntStart;[None]");
+//		communication.getReturn("0;0;i;cmd;getCluesLeft;[None]");
+//		communication.getReturn("0;0;i;cmd;getStepsLeft;[None]");
+//		communication.getReturn("0;0;i;cmd;huntFight;[None]");
 
 		// /**
 		// * ChangeMapTest method Input : North, South, East, West
