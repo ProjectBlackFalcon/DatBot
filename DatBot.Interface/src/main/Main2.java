850428e28c8471822f55c9e160b3dcf01d20acb4
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-02-24 12:50:16 +0100

    Gen jar

---
+++ DatBot.Interface/src/main/Main2.java
@@ -0,0 +1,27 @@
+package main;
+
+import main.communication.Communication;
+import protocol.network.Network;
+import utils.GameData;
+import utils.d2i.d2iManager;
+import utils.d2p.MapManager;
+
+public class Main2 {
+
+	public static void main(String[] args) throws Exception {
+
+		new d2iManager(GameData.getPathDatBot() + "\\DatBot.Interface\\utils\\gamedata\\i18n_fr.d2i");
+		new MapManager(GameData.getPathDatBot() + "\\DatBot.Interface\\utils\\maps");
+
+		boolean arg = false;
+		if (args.length != 0) {
+			if ((args[0].equals("true") || args[0].equals("True"))) {
+				arg = true;
+			}
+		}
+
+		Communication communication = new Communication(arg);
+		Thread communication2 = new Thread(communication);
+		communication2.start();
+	}
+}
