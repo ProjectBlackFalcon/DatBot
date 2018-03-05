cd33840874ccf289bb9e0aeecfb3c5f54bd3bc77
Baptiste Beduneau <b.beduneau@innoseis.com>
2018-03-05 16:06:18 +0100

    Fix openDD

--- DatBot.Interface/src/game/plugin/Dragodinde.java
+++ DatBot.Interface/src/game/plugin/Dragodinde.java
@@ -61,28 +61,34 @@ public class Dragodinde {
 				else
 					s += "\"" + GameData.getMountBehaviorName(paddock.get(i).getBehaviors().get(i1)) + "\",";
 			}
-			s += "]";
-			s += "\"name\":" + stable.get(i).getName() + "," ;
-			s += "\"sex\":" + stable.get(i).isSex() + "," ;
-			s += "\"experience\":" + stable.get(i).getExperience() + "," ;
-			s += "\"level\":" + stable.get(i).getLevel() + "," ;
-			s += "\"stamina\":" + stable.get(i).getStamina() + "," ;
-			s += "\"staminaMax\":" + stable.get(i).getStaminaMax() + "," ;
-			s += "\"maturity\":" + stable.get(i).getMaturity() + "," ;
-			s += "\"maturityForAdult\":" + stable.get(i).getMaturityForAdult() + "," ;
-			s += "\"energy\":" + stable.get(i).getEnergy() + "," ;
-			s += "\"energyMax\":" + stable.get(i).getEnergyMax() + "," ;
-			s += "\"serenity\":" + stable.get(i).getSerenity() + "," ;
-			s += "\"serenityMax\":" + stable.get(i).getSerenityMax() + "," ;
-			s += "\"aggressivityMax\":" + stable.get(i).getAggressivityMax() + "," ;
-			s += "\"love\":" + stable.get(i).getLove() + "," ;
-			s += "\"loveMax\":" + stable.get(i).getLoveMax() + "," ;
-			s += "\"fecondationTime\":" + stable.get(i).getFecondationTime() + "," ;
-			s += "\"isFecondationReady\":" + stable.get(i).isIsFecondationReady() + "," ;
-			s += "\"boostLimiter\":" + stable.get(i).getBoostLimiter() + "," ;
-			s += "\"boostMax\":" + (int) stable.get(i).getBoostMax() + "," ;
-			s += "\"reproductionCount\":" + stable.get(i).getReproductionCount() + "," ;
-			s += "\"reproductionCountMax\":" + stable.get(i).getReproductionCountMax() + "," ;
+			s += "],";
+			s += "\"name\":\"" + paddock.get(i).getName() + "\"," ;
+			if(paddock.get(i).isSex())
+				s += "\"sex\":True," ;
+			else 
+				s += "\"sex\":False," ;
+			s += "\"experience\":" + paddock.get(i).getExperience() + "," ;
+			s += "\"level\":" + paddock.get(i).getLevel() + "," ;
+			s += "\"stamina\":" + paddock.get(i).getStamina() + "," ;
+			s += "\"staminaMax\":" + paddock.get(i).getStaminaMax() + "," ;
+			s += "\"maturity\":" + paddock.get(i).getMaturity() + "," ;
+			s += "\"maturityForAdult\":" + paddock.get(i).getMaturityForAdult() + "," ;
+			s += "\"energy\":" + paddock.get(i).getEnergy() + "," ;
+			s += "\"energyMax\":" + paddock.get(i).getEnergyMax() + "," ;
+			s += "\"serenity\":" + paddock.get(i).getSerenity() + "," ;
+			s += "\"serenityMax\":" + paddock.get(i).getSerenityMax() + "," ;
+			s += "\"aggressivityMax\":" + paddock.get(i).getAggressivityMax() + "," ;
+			s += "\"love\":" + paddock.get(i).getLove() + "," ;
+			s += "\"loveMax\":" + paddock.get(i).getLoveMax() + "," ;
+			s += "\"fecondationTime\":" + paddock.get(i).getFecondationTime() + "," ;
+			if(paddock.get(i).isIsFecondationReady())
+				s += "\"isFecondationReady\":True," ;
+			else 
+				s += "\"isFecondationReady\":False," ;
+			s += "\"boostLimiter\":" + paddock.get(i).getBoostLimiter() + "," ;
+			s += "\"boostMax\":" + (int) paddock.get(i).getBoostMax() + "," ;
+			s += "\"reproductionCount\":" + paddock.get(i).getReproductionCount() + "," ;
+			s += "\"reproductionCountMax\":" + paddock.get(i).getReproductionCountMax() + "," ;
 			s += "\"inPaddock\":" + "True" ;
 			s += "}";
 			if(i != paddock.size() - 1){
@@ -104,9 +110,12 @@ public class Dragodinde {
 				else
 					s += "\"" + GameData.getMountBehaviorName(stable.get(i).getBehaviors().get(i1)) + "\",";
 			}
-			s += "]";
-			s += "\"name\":" + stable.get(i).getName() + "," ;
-			s += "\"sex\":" + stable.get(i).isSex() + "," ;
+			s += "],";
+			s += "\"name\":\"" + stable.get(i).getName() + "\"," ;
+			if(stable.get(i).isSex())
+				s += "\"sex\":True," ;
+			else 
+				s += "\"sex\":False," ;
 			s += "\"experience\":" + stable.get(i).getExperience() + "," ;
 			s += "\"level\":" + stable.get(i).getLevel() + "," ;
 			s += "\"stamina\":" + stable.get(i).getStamina() + "," ;
@@ -121,12 +130,15 @@ public class Dragodinde {
 			s += "\"love\":" + stable.get(i).getLove() + "," ;
 			s += "\"loveMax\":" + stable.get(i).getLoveMax() + "," ;
 			s += "\"fecondationTime\":" + stable.get(i).getFecondationTime() + "," ;
-			s += "\"isFecondationReady\":" + stable.get(i).isIsFecondationReady() + "," ;
+			if(stable.get(i).isIsFecondationReady())
+				s += "\"isFecondationReady\":True," ;
+			else 
+				s += "\"isFecondationReady\":False," ;
 			s += "\"boostLimiter\":" + stable.get(i).getBoostLimiter() + "," ;
 			s += "\"boostMax\":" + (int) stable.get(i).getBoostMax() + "," ;
 			s += "\"reproductionCount\":" + stable.get(i).getReproductionCount() + "," ;
 			s += "\"reproductionCountMax\":" + stable.get(i).getReproductionCountMax() + "," ;
-			s += "\"inPaddock\":" + "False" ;
+			s += "\"inPaddock\":" + "True" ;
 			s += "}";
 			if(i != stable.size() - 1){
 				s += ",";
