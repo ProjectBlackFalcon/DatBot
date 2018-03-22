9b7e84976951d344e745f0fbb4ff517100041333
Ugdha <alexisvlrt@gmail.com>
2018-03-22 19:11:51 +0100

    I fucked something up, fixed

--- Model/HighLevelFunctions.py
+++ Model/HighLevelFunctions.py
@@ -718,6 +718,7 @@ class HighLevelFunctions:
             if schedule_name is None:
                 self.bot.schedule = self.llf.get_schedule(self.bot.credentials['name'])
                 schedule = self.bot.schedule
+
             caught_up = False
             for task in schedule:
                 if (time.localtime().tm_hour + time.localtime().tm_min/60) - task['end'] < 0 or caught_up:
