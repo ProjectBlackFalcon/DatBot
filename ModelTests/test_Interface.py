2fee7e6bf634853f45821187b375ed043ecbe921
Ugdha <alexisvlrt@gmail.com>
2018-06-20 18:25:05 +0200

    New tests

--- ModelTests/test_Interface.py
+++ ModelTests/test_Interface.py
@@ -4,16 +4,19 @@ from Bot import Bot
 import unittest
 import warnings
 
+test_account_credentials = {'username': 'democraticamnesiac', 'password': 'answerflash2', 'name': 'Maxitreur', 'server': 'Julith'}
 
-class Connection(unittest.TestCase):
-    def setUp(self):
+
+class a_Connection(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
         warnings.simplefilter("ignore")
         llf = LowLevelFunctions()
         self.pipe = PipeToJava(headless=True)
         self.bot = Bot(self.pipe, 0, test_account_credentials, llf, True)
 
-    def test_connect(self):
-        self.assertEqual([True], self.bot.interface.connect())
+    def test_a_connect(self):
+        self.assertEqual(True, self.bot.interface.connect()[0])
         self.assertTrue(self.bot.connected)
         self.assertEqual('Connecting', self.bot.occupation)
         self.assertIsNotNone(self.bot.position)
@@ -22,78 +25,80 @@ class Connection(unittest.TestCase):
         self.assertFalse(self.bot.in_fight)
         self.assertIsNone(self.bot.mount)
 
-    def test_disconnect(self):
-        self.assertEqual([True], self.bot.interface.disconnect())
+    def test_b_disconnect(self):
+        self.assertEqual(True, self.bot.interface.disconnect()[0])
         self.assertFalse(self.bot.connected)
         self.assertEqual('Sleeping', self.bot.occupation)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.pipe.p.terminate()
 
 
-class MoveAround(unittest.TestCase):
-    def setUp(self):
+class b_MoveAround(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
         warnings.simplefilter("ignore")
         llf = LowLevelFunctions()
         self.pipe = PipeToJava(headless=True)
         self.bot = Bot(self.pipe, 0, test_account_credentials, llf, True)
         self.bot.interface.connect(max_tries=1)
 
-    def test_get_map(self):
+    def test_a_get_map(self):
         map = self.bot.interface.get_map()
         self.assertEqual((4, -3), map[0])
         self.assertEqual(300, map[1])
         self.assertEqual(2, map[2])
 
-    def test_move(self):
-        self.assertEqual([True], self.bot.interface.move(255))
-        self.assertEqual([False], self.bot.interface.move(-1))
+    def test_b_move(self):
+        self.assertEqual(True, self.bot.interface.move(255)[0])
+        self.assertEqual(False, self.bot.interface.move(171)[0])
 
-    def test_change_map(self):
-        self.assertEqual([False], self.bot.interface.change_map(255, 'n'))
-        self.assertEqual([True], self.bot.interface.change_map(364, 'w'))
-        self.assertEqual([True], self.bot.interface.change_map(321, 'e'))
+    def test_c_change_map(self):
+        self.assertEqual(False, self.bot.interface.change_map(255, 'n')[0])
+        self.assertEqual(True, self.bot.interface.change_map(364, 'w')[0])
+        self.assertEqual(True, self.bot.interface.change_map(321, 'e')[0])
 
-    def test_go_to_astrub(self):
+    def test_d_go_to_astrub(self):
         self.bot.interface.go_to_astrub()
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((6, -19), curr_map)
         self.assertEqual(397, cell)
         self.assertEqual(1, worldmap)
 
-    def test_go_to_incaranm(self):
+    def test_e_go_to_incaranm(self):
         self.bot.interface.go_to_incarnam()
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((3, -4), curr_map)
         self.assertEqual(300, cell)
         self.assertEqual(2, worldmap)
 
-    def test_heavenbag(self):
-        self.assertEqual([True], self.bot.interface.enter_heavenbag())
+    def test_f_heavenbag(self):
+        self.assertEqual(True, self.bot.interface.enter_heavenbag()[0])
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((0, 0), curr_map)
         self.assertEqual(324, cell)
         self.assertEqual(-1, worldmap)
 
-        self.assertEqual([True], self.bot.interface.enter_heavenbag())
+        self.assertEqual(True, self.bot.interface.enter_heavenbag()[0])
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((0, 0), curr_map)
         self.assertEqual(324, cell)
         self.assertEqual(-1, worldmap)
 
-        self.assertEqual([True], self.bot.interface.exit_heavenbag())
+        self.assertEqual(True, self.bot.interface.exit_heavenbag()[0])
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((3, -4), curr_map)
         self.assertEqual(300, cell)
         self.assertEqual(2, worldmap)
 
-        self.assertEqual([True], self.bot.interface.exit_heavenbag())
+        self.assertEqual(True, self.bot.interface.exit_heavenbag()[0])
         curr_map, cell, worldmap, map_id = self.bot.interface.get_map()
         self.assertEqual((3, -4), curr_map)
         self.assertEqual(300, cell)
         self.assertEqual(2, worldmap)
 
-    def test_zaap(self):
+    def test_g_zaap(self):
         self.bot.interface.go_to_astrub()
         self.bot.interface.enter_heavenbag()
 
@@ -105,18 +110,21 @@ class MoveAround(unittest.TestCase):
         self.assertEqual(260, cell)
         self.assertEqual(1, worldmap)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.pipe.p.terminate()
 
 
-class Bank(unittest.TestCase):
-    def setUp(self):
+class c_Bank(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
         warnings.simplefilter("ignore")
         llf = LowLevelFunctions()
         self.pipe = PipeToJava(headless=True)
         self.bot = Bot(self.pipe, 0, test_account_credentials, llf, False)
         self.bot.interface.connect(max_tries=1)
-        self.assertEqual((4, -18), self.bot.position[0])
+        print(self.bot.position[0])
+        assert (4, -18) == self.bot.position[0]
 
     def test_enter_bank(self):
         self.bot.interface.enter_bank()
@@ -126,13 +134,12 @@ class Bank(unittest.TestCase):
     def test_open_bank(self):
         self.assertEqual(dict, type(self.bot.interface.open_bank()[0]))
 
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         pass
 
 
 if __name__ == '__main__':
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # INITAL TESTING CONDITIONS FOR TEST SUITE 1
     #
@@ -143,11 +150,8 @@ if __name__ == '__main__':
     # Known zaaps                                        Astrub
     # Level                                                > 10
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    test_account_credentials = {'username': 'wublel2', 'password': 'notabot0', 'name': 'Gradopr', 'server': 'Julith'}
     runner = unittest.TextTestRunner()
-    runner.run(Connection())
-    # runner.run(move_around())
+    runner.run(b_MoveAround)
 
 
 __author__ = 'Alexis'
---
+++ ModelTests/test_Pathfinder.py
@@ -0,0 +1,50 @@
+from Pathfinder import PathFinder
+import numpy as np
+import winsound
+
+
+def generate_map_collage():
+    maps_coords = pf.get_maps_coords()
+    maps = []
+    shape = (abs(end[1] - start[1]) + 1, abs(end[0] - start[0]) + 1)
+    counter = 0
+    for coord in maps_coords:
+        map_infos = pf.llf.coord_fetch_map(coord, pf.worldmap)
+        counter += 1
+        print('{}/{}'.format(counter, shape[0]*shape[1]))
+        if map_infos is not None and np.array(map_infos).shape == (40, 14):
+            maps.append(map_infos)
+        elif map_infos is not None and np.array(map_infos).shape != (40, 14):
+            maps.append([[5]*14]*40)
+        else:
+            maps.append([[1] * 14] * 40)
+    glued = pf.glue_maps(maps, shape)
+    # print(glued)
+    # print(pf.adapted_maps)
+    pf.map_to_image(pf.adapt_shape_maps(glued), 1)
+
+
+def generate_path():
+    pf.get_path()
+    pf.add_path_to_adapted_maps()
+    pf.add_map_change_coords_to_adapted_maps()
+    pf.map_to_image(pf.adapted_maps, 1)
+    print(pf.path_cells)
+    print(pf.get_map_change_cells())
+    return(pf.get_map_change_cells())
+
+lel = [(4, -19), (-5, -23), (-13, -28), (-3, -42), (-17, -47), (-32, -56), (-27, -36), (-20, -20), (-16, 1), (-25, 12), (-15, 25), (-26, 35)]
+
+start = (4, -19)
+end = (-5, -23)
+worldmap = 1
+pf = PathFinder(start, end, None, None, worldmap)
+path = generate_path()
+
+
+print('Done !')
+winsound.PlaySound('..//Utils//sound.wav', winsound.SND_FILENAME)
+
+
+__author__ = 'Alexis'
+
