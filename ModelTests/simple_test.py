c34910d3aaf95524b46c4147666eff873c61a171
Ugdha <alexisvlrt@gmail.com>
2018-06-29 19:27:25 +0200

    Fix in hunt recap upload

--- ModelTests/simple_test.py
+++ ModelTests/simple_test.py
@@ -10,5 +10,8 @@ bot = Bot(pipe, 0, test_account_credentials, llf, True)
 
 bot.interface.connect()
 time.sleep(2)
+with open('../Utils/HuntLogs.txt', 'r') as f:
+    log = ''.join(f.readlines())
+bot.llf.hunts_to_db('Test', 0, True, log, 'test')
 bot.interface.disconnect()
 pipe.p.terminate()
