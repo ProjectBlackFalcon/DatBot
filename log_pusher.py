d5b2a23d9232fa91f04ff7d41ab2ff7d9ff8001e
Ugdha <alexisvlrt@gmail.com>
2018-06-29 10:48:08 +0200

    Script to manually push master and network logs to the database

---
+++ log_pusher.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import Database_credentials as dc
+
+with open('Utils/BotsLogs/Maxitreur.txt', 'r') as f:
+    master_log = ''.join(f.readlines()).replace("'", '"')
+
+with open('log_network0.txt', 'r') as f:
+    net_log = ''.join(f.readlines()).replace("'", '"')
+
+conn = mysql.connector.connect(host=dc.host, user=dc.user, password=dc.password, database=dc.database)
+cursor = conn.cursor()
+cursor.execute("""INSERT INTO ManualLogs (master, network) VALUES ('{}', '{}')""".format(master_log, net_log))
+conn.commit()
+conn.close()
