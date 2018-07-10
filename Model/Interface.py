ecc2a51bf5acd22d49fb7b69ecde683610eb5ad8
Ugdha <alexisvlrt@gmail.com>
2018-07-10 22:45:21 +0200

    Added auto-mounting and auto XPing of equipped DD. Implemented waiting time before taking a new hunt

--- Model/Interface.py
+++ Model/Interface.py
@@ -55,6 +55,7 @@ class Interface:
         self.pipe = self.bot.pipe  # type: PipeToJava
         self.current_id = 0
         self.bank_info = {}
+        self.new_hunt_timer = 0
         self.color = color
         self.end_color = '\033[0m'
 
@@ -150,8 +151,14 @@ class Interface:
                 self.get_player_stats()
                 current_map, current_cell, current_worldmap, map_id = self.bot.interface.get_map()
                 self.bot.position = (current_map, current_worldmap)
-                if self.get_dd_stat()[0]:
+                dd_stats = self.get_dd_stat()
+                if dd_stats[0]:
                     self.bot.mount = 'equipped'
+                    self.mount_dd()
+                    if dd_stats[0] < 100:
+                        self.set_dd_xp(90)
+                    else:
+                        self.set_dd_xp(0)
                 else:
                     self.bot.mount = self.bot.llf.get_mount_situation(self.bot.credentials['name'])
                     if self.bot.mount == 'resting':
@@ -423,10 +430,15 @@ class Interface:
         :param level:
         :return: Boolean
         """
+        if time.time()-self.new_hunt_timer < 10*60:
+            time.sleep(10*60 - time.time() + self.new_hunt_timer + 30)
         if level == 'max':
-            return self.execute_command('newHunt')
+            succes = self.execute_command('newHunt')
         else:
-            return self.execute_command('newHunt', [level])
+            succes = self.execute_command('newHunt', [level])
+        if succes[0]:
+            self.new_hunt_timer = time.time()
+        return succes
 
     def hunt_is_active(self):
         """
