7afadc314cad6e4da81ef6e7c862d4dfefad2869
Ugdha <alexisvlrt@gmail.com>
2018-10-02 21:38:51 +0200

    Whole lot of data analysis

--- Model/DataScienceShit.py
+++ Model/DataScienceShit.py
@@ -3,6 +3,7 @@ from Resources import Resources
 import Database_credentials as dc
 import mysql.connector
 from Item import Item
+import time
 
 
 class Database:
@@ -27,7 +28,29 @@ class Database:
         output = pd.DataFrame(rows, columns=['Time', 'ItemId', 'Price1', 'Price10', 'Price100', 'PriceAvg'])
         output['Name'] = output['ItemId'].apply(lambda itemid: self.resources.id2names[str(itemid)])
         output.set_index('ItemId', drop=True, inplace=True)
+        output = output.loc[~output.index.duplicated(keep='first')]
         return output
+
+    def get_item_from_id(self, item_id, server):
+        conn = mysql.connector.connect(host=dc.host, user=dc.user, password=dc.password, database=dc.database)
+        cursor = conn.cursor()
+        cursor.execute("""  
+                    SELECT TimeIn, ItemId, Price1, Stats, Hash
+                    FROM ItemPrices
+                    WHERE TimeIn IN (
+                        SELECT MAX(TimeIn)
+                        FROM ItemPrices
+                        WHERE ItemId = {} AND Server = '{}'
+                    ) AND ItemId = {}
+                """.format(item_id, server, item_id))
+        rows = cursor.fetchall()
+        output = pd.DataFrame(rows, columns=['Time', 'ItemId', 'Price', 'Stats', 'Hash'])
+        output['Name'] = output['ItemId'].apply(lambda itemid: self.resources.id2names[str(itemid)])
+        output.set_index('ItemId', drop=True, inplace=True)
+        return output
+
+    def get_runes_prices(self, server):
+        return self.resource_price_from_db(server, self.resources.runes_ids.values())
 
 
 class DS:
@@ -35,7 +58,8 @@ class DS:
         self.resources = resources
         self.database = Database(resources)
 
-    def estimate_craft_cost(self, item_id_list, server):
+    def estimate_craft_cost(self, item_id_list, server, rich_return=False):
+        item_id_list = [item_id_list] if type(item_id_list) is not list else item_id_list
         recipes = []
         ingredients = {}
         for recipe in self.resources.recipes:
@@ -48,18 +72,64 @@ class DS:
                         ingredients[ingredient] = [quantity, -1]
 
         recipes = pd.DataFrame(recipes).set_index('resultId', drop=True)
+        prices = self.database.resource_price_from_db(server, ingredients.keys())['PriceAvg']
+        items_in_recipe_with_price = {}
+        for item_id in ingredients.keys():
+            if str(item_id) in self.resources.equipments:
+                data = self.database.get_item_from_id(item_id, server).sort_values('Price')
+                if len(data):
+                    items_in_recipe_with_price[item_id] = data.Price.iloc[0]
+        prices = prices.append(pd.Series(items_in_recipe_with_price))
+
         ingredients = recipes['Ingredients'].apply(lambda ingredients: pd.DataFrame(ingredients, columns=['ItemId', 'Quantity']).set_index('ItemId', drop=True).sort_index())
-        prices = ingredients.apply(lambda recipe: self.database.resource_price_from_db(server, recipe.index.tolist())['PriceAvg'])
 
         quantities = pd.DataFrame(ingredients.apply(lambda item: item['Quantity'])).T
-        price_per_ingredient = prices.T.mul(quantities)
+        price_per_ingredient = quantities.T.mul(prices).T
 
         for i in range(len(ingredients)):
             ingredients.iloc[i]['Price'] = price_per_ingredient.iloc[:, i].dropna()
         recipes['Price'] = price_per_ingredient.sum()
-        return recipes
+
+        if rich_return:
+            return recipes
+        elif len(recipes) == 1:
+            return int(recipes.Price)
+        else:
+            return list(recipes.Price)
+
+    def item_breaking_analysis(self, item_id_list, server):
+        item_id_list = [item_id_list] if type(item_id_list) is not list else item_id_list
+        runes_prices = ds.database.get_runes_prices(server)
+        craft_costs = ds.estimate_craft_cost(item_id_list, 'Julith', rich_return=True)
+        report = []
+        non_craftable = 0
+        for item_id in item_id_list:
+            print('Running simulation for item {}/{}'.format(item_id_list.index(item_id) + 1, len(item_id_list)))
+            try:
+                item = Item(self.resources, item_id=item_id, creation_price=craft_costs.loc[item_id]['Price'])
+                item.generate_random_instance()
+                min_coeff, focus = item.get_min_coeff(runes_prices)
+                report.append([item.item_name, int(item.level), int(craft_costs.loc[item_id]['Price']), int(min_coeff), focus])
+            except:
+                non_craftable += 1
+                print(self.resources.id2names[str(item_id)])
+        report = pd.DataFrame(report, columns=['Item Name', 'Level', 'Craft Price', 'Min coeff', 'Focus']).set_index('Item Name')
+        print(non_craftable)
+        return report
+
+    def get_item_ids_from_level(self, lvl_min, lvl_max):
+        item_ids = []
+        for item_id, values in self.resources.equipments.items():
+            if lvl_min <= values['Level'] <= lvl_max:
+                item_ids.append(int(item_id))
+        return item_ids
 
 
 if __name__ == '__main__':
     ds = DS(Resources())
-    print(ds.estimate_craft_cost([958, 2629, 6805, 8287], 'Julith'))
+    start = time.time()
+    ids = ds.get_item_ids_from_level(151, 165)
+    print(ids)
+    report = ds.item_breaking_analysis(ids, 'Julith').sort_values(by=['Min coeff'], ascending=False)
+    report.to_csv('RuneReport.csv')
+    print(time.time() - start)
--- Model/Item.py
+++ Model/Item.py
@@ -1,20 +1,22 @@
 import json
 import zlib
-
+from random import randint
 
 class Item:
-    def __init__(self, resources, item_stats, item_price=-1, item_id=None, level=-1, creation_price=-1, resource_prices=None):
+    def __init__(self, resources, item_stats=None, item_price=-1, item_id=None, level=None, creation_price=-1, resource_prices=None):
         self.all_stats = ['PM', 'Résistance Terre JCJ', 'Résistance Eau JCJ', 'Résistance Air JCJ', 'Résistance Feu JCJ', 'Résistance Neutre JCJ', 'Puissance', 'Sagesse', 'Dommages Neutre', 'Retrait PA', 'Retrait PM', 'Pods', 'Esquive PA', 'Esquive PM', 'Dommages Critiques', 'Résistance Critiques', 'Dommages Terre', 'Résistance Eau', 'Dommages Feu', 'Dommages', 'Dommages Eau', 'Dommages Air', 'Résistance Terre', 'Initiative', 'Vitalité', 'Prospection', 'Soins', '% Critique', 'Dommages Poussée', 'Invocations', '% Dommages distance', 'Résistance Poussée', '% Dommages mêlée', '% Résistance distance', 'Intelligence', '% Résistance Terre', '% Résistance Eau', '% Résistance Air', '% Résistance Feu', '% Résistance Neutre', '% Résistance mêlée', 'Dommages Pièges', 'Chance', '% Résistance Air JCJ', 'Vie', 'PA', 'Fuite', 'Tacle', 'Résistance Air', 'Résistance Feu', 'Résistance Neutre', 'Portée', 'Force', 'Agilité', "% Dommages d'armes", '% Résistance Terre JCJ', '% Résistance Eau JCJ', '% Dommages aux sorts', '% Résistance Feu JCJ', '% Résistance Neutre JCJ']
 
         self.resources = resources
-        self.stats = item_stats if type(item_stats) == dict else self.stats2dict(item_stats)
-        self.level = level
+        self.fm_stats = None
+        self.stats = self.stats2dict(item_stats) if type(item_stats) == list else item_stats
+        self.level = resources.equipments[str(item_id)]['Level'] if level is not None else None
         self.creation_price = creation_price
         self.resource_prices = resource_prices
-        self.item_name = resources.id2names[item_id] if item_id is not None else ''
+        self.item_name = resources.id2names[str(item_id)] if item_id is not None else ''
         self.price = item_price
-        with open('../Utils/RunesStats.json', 'r') as f:
-            self.runes_stats = json.load(f)
+        self.item_id = item_id
+        self.potential_stats = resources.equipments[str(item_id)] if item_id is not None else None
+        self.runes_stats = resources.runes_stats
         self.coeff = 100
 
     def __str__(self):
@@ -23,6 +25,13 @@ class Item:
     def __hash__(self):
         return zlib.adler32(bytes(self.__str__(), 'utf-8'))
 
+    def generate_random_instance(self):
+        if self.item_id is None:
+            raise ValueError('Item id must be specified')
+        generated_stat_list = [(stat, randint(mn, mx)) for stat, mn, mx in self.potential_stats['Stats']]
+        self.level = self.resources.equipments[str(self.item_id)]['Level']
+        self.stats2dict(generated_stat_list)
+
     def stats2dict(self, stats):
         stats = [stat for stat in stats if len(stat) == 2 and stat[0] != 740]
         #                                                                         Look at this shit
@@ -30,31 +39,42 @@ class Item:
         for stat in self.resources.effect_id2name.values():
             if stat not in output.keys():
                 output[stat] = 0
-        return output
+        self.stats = output
+        self.fmable_stats()
+        return self.stats
+
+    def fmable_stats(self):
+        if type(self.stats) is dict:
+            self.fm_stats = {stat: value for stat, value in self.stats.items() if stat in self.resources.fmable_stats.keys()}
+            return self.fm_stats
 
     def get_rune_weights(self):
-        return {stat: self.runes_stats[stat][0] if stat not in ['Vitalite', 'Pods', 'Initiative'] else 1 for stat, value in self.stats.items() if self.stats[stat]}
+        return {stat: self.runes_stats[stat][0] if stat not in ['Vitalité', 'Pods', 'Initiative'] else 1 for stat, value in self.fm_stats.items() if self.fm_stats[stat]}
 
     def get_local_focus_weights(self):
-        return {stat: value * self.runes_stats[stat][0] for stat, value in self.stats.items() if value}
+        return {stat: value * self.runes_stats[stat][0] for stat, value in self.fm_stats.items() if value}
 
     def get_min_coeff(self, rune_prices):
         with_focus, no_focus = self.get_runes_obtained()
-        max_value = sum([price[3]*no_focus[rune] for rune, price in rune_prices.items()])
+        rune_prices = rune_prices[~rune_prices['Name'].str.contains(' Pa | Ra | de ')]
+        stat_price = {stat: rune_prices.loc[rune, 'PriceAvg'] for rune in rune_prices.index.tolist() for stat, (_, runes_ids) in self.resources.fmable_stats.items() if rune in runes_ids}
+        max_value = sum([stat_price[rune]*qty for rune, qty in no_focus.items()])
         focus = 'No focus'
 
         for rune, number in with_focus.items():
-            if rune_prices[rune][3] * number > max_value:
+            if stat_price[rune] * number > max_value:
                 focus = rune
-                max_value = rune_prices[rune][3] * number
+                max_value = stat_price[rune] * number
 
-        min_coeff = self.creation_price / max_value
+        min_coeff = round(100 * self.creation_price / max_value)
         return min_coeff, focus
 
     def get_runes_obtained(self):
+        if self.level == -1:
+            raise ValueError('Level must be specified')
         weights = self.get_rune_weights()
         local_focus_weights = self.get_local_focus_weights()
-        total_weight = sum([value * self.runes_stats[stat][0] for stat, value in self.stats.items()])
+        total_weight = sum([value * self.runes_stats[stat][0] for stat, value in self.fm_stats.items()])
 
         with_focus = {stat: ((value+(total_weight-value)/2)/weights[stat])*(self.level*0.025)*(self.coeff/100)*0.55 for stat, value in local_focus_weights.items()}
         with_focus = {stat: int(value) if stat in ['PA', 'PM', 'Portée'] else round(value, 1) for stat, value in with_focus.items()}
